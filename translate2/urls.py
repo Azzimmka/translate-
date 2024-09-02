503a1ec2cd1eebf2c2f99882226e8ec10e73a21b
azim gulyamov <azimgulyamov@192.168.1.2>
2024-09-02 04:42:28 -0700

    закончили selector

--- translate2/urls.py
+++ translate2/urls.py
@@ -22,5 +22,6 @@ urlpatterns = i18n_patterns (
     path('admin/', admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
     path('', include('core.urls')),
+    prefix_default_language=False,
 )
 
