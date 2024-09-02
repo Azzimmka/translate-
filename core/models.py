807f8b7368bb382b6249ce357b093bfe4c9c9357
azim gulyamov <azimgulyamov@192.168.1.2>
2024-09-02 00:08:39 -0700

    закончили первый раздел, начали перевод time zone 17:50

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title=models.CharField(max_length=50)
+    content=models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
